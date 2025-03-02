d6600f35ee8fd35ad2a99bdcfb1d148a8d1896d8
viji2ks <35405451+viji2ks@users.noreply.github.com>
2025-03-02 09:46:45 +0530

    Update MillisTimerLib.cpp
    //example usage
    
    timer.timer()   //return bool normal continuos running timer.timer(false)      //stop timer added curstomized
    timer.timer(true)      // enable or on timer added  customized
    
    
    // usage 
    timer.reset();  // reset with previous declared timer value
    timer.reset(long delayTime)  // reset with new curtomized timer value 
    
    //exmple usage
    timer.reset()  // normal usage as per owner prog
    timer.reset(3000);  // set timer to 3 sec with old time replaced

--- MillisTimerLib.cpp
+++ MillisTimerLib.cpp
@@ -5,24 +5,47 @@
   Author   : Fellipe Couto [ http://www.efeitonerd.com.br ]
   GitHub   : https://github.com/fellipecouto/MillisTimerLib
   Date     : 2020-06-07
+
+  new added enable disable timer using timer.reset()  // timer.timer() return true/false asper timeout
+  timer.reset(true)  //timer as per timeout true or false , timer running
+  timer.reset(false)  //timer always false no action  , but timer running
+
+  timer.reset();  old-timer set by intialize setup loop object
+  timer.reset(long set-time);  resets timer along new set-timer
   -----------------------------------------------------------------------------------*/
 #include "MillisTimerLib.h"
 #include <Arduino.h>
 
 unsigned long _oldTime = 0;
 unsigned long _time = 0;
+bool _on-off = true; //auto timer() default
+
+bool _on-off = true;    // key bool for timer out enable other wise can't stop timer.timer() return bool; 
+
 
 MillisTimerLib::MillisTimerLib(long delayTime) {
   setDelay(delayTime);
 }
 
 bool MillisTimerLib::timer() {
+  _on-off = en-time
   if (timerCount() >= _time) {
       reset();
-      return true;
+      return true & _on-off;
     }
-  return false;
+  return false & _on-off;
 }
+
+bool MillisTimerLib::timer(bool en-timer) {
+  _on-off = en-timer
+  if (timerCount() >= _time) {
+      reset();
+      return true & _on-off;
+    }
+  return false & _on-off;
+}
+
+
 
 long MillisTimerLib::timerCount() {
   return millis() - _oldTime;
@@ -43,3 +66,13 @@ long MillisTimerLib::getDelay() {
 void MillisTimerLib::reset() {
   _oldTime = millis();
 }
+
+void MillisTimerLib::reset(long delayTime) {
+  _oldTime = millis();
+   if (delayTime > 0) {
+    _time = delayTime;
+  } else {
+    _time = 0;
+  }
+}
+
